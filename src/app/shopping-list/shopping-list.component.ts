595da4ea35b13fe798585f66292901397902f6e0
mustafa <mustafa.jabbar@dcs-fuerth.de>
2018-11-28 15:35:12 +0100

    work 28/11/2018

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/Ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,10 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
-
+  Ingredients:Ingredient[] = [
+    new Ingredient('apples',5),
+    new Ingredient('Tamatoes',10),
+    new Ingredient('Potatoes',6),
+    new Ingredient('Onions',3),
+    new Ingredient('garlic',1),
+  ];
   constructor() { }
 
   ngOnInit() {
   }
 
 }
+
