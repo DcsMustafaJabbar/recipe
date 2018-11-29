8f8f80e2b5b413ff3bb89ab6ac5266a27cc8a27b
mustafa <mustafa.jabbar@dcs-fuerth.de>
2018-11-29 09:52:54 +0100

    29.11.2018

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -7,10 +7,14 @@ import { recipe } from '../recipes.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-recipes:recipe[] = [new recipe('test recipe','the simpel test','https://cdn.shopify.com/s/files/1/0662/0785/products/recipe_cover_grande.jpg?v=1493774901')];
+  recipes: recipe[] = [new recipe('Fish with Lemon', 'the Fish with Lemo', 'http://www.chewoutloud.com/wp-content/uploads/2017/06/easy-lemon-butter-fish-0.jpg'),
+  new recipe('Chicken', 'the Chicken', 'https://www.seriouseats.com/recipes/images/2014/09/20141001-grilled-spiced-cauliflower-joshua-bousel-1500x1125.jpg'),
+  new recipe('Taka Chicken', 'the Taka Chicken', 'https://c1.staticflickr.com/3/2809/33354707111_28840a00d1_z.jpg'),
+  new recipe('Nudell', 'the Nudell', 'https://pinchofyum.com/wp-content/uploads/Chili-Sesame-Zoodles-Recipe.jpg')];
   constructor() { }
 
   ngOnInit() {
   }
 
 }
+
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/Ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,10 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
-
+  Ingredients:Ingredient[] = [
+    new Ingredient('apples',5),
+    new Ingredient('Tamatoes',10),
+    new Ingredient('Potatoes',6),
+    new Ingredient('Onions',3),
+    new Ingredient('garlic',1),
+    new Ingredient('paprka',4),
+  ];
   constructor() { }
 
   ngOnInit() {
   }
 
 }
+
